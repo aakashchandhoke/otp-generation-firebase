246c0a7c28232ef0bc73f679fcc8b7a846320378
aakash chandhoke <aakash.chandhoke24@gmail.com>
2020-09-08 23:32:03 +0530

    Updates

---
+++ precache-manifest.ded00e8d2f2aadf4cdcc390a6182d0cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4d8cccfd898749117808f9dbb8a61e",
+    "url": "/otp-generation-firebase/index.html"
+  },
+  {
+    "revision": "fef35b2cb7097661522e",
+    "url": "/otp-generation-firebase/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "f31eae5d0fa10bff2873",
+    "url": "/otp-generation-firebase/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fef35b2cb7097661522e",
+    "url": "/otp-generation-firebase/static/js/2.fc5e6dc3.chunk.js"
+  },
+  {
+    "revision": "77548c0d66ac42292abfda2c9c306920",
+    "url": "/otp-generation-firebase/static/js/2.fc5e6dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31eae5d0fa10bff2873",
+    "url": "/otp-generation-firebase/static/js/main.689e4929.chunk.js"
+  },
+  {
+    "revision": "88aa24d30bb2f5234944",
+    "url": "/otp-generation-firebase/static/js/runtime-main.f8de929c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c22552dfbc3f8d588fc9b01bb071960.js"
+  "/otp-generation-firebase/precache-manifest.ded00e8d2f2aadf4cdcc390a6182d0cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/otp-generation-firebase/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
